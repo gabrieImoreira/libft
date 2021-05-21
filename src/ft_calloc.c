fe4d4e5a7e84d044c4a6c71e22d2c977fc8ad663
gams <gabriel.antonioms@gmail.com>
2021-05-20 22:35:54 -0300

    correct substr

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/20 19:02:48 by gantonio          #+#    #+#             */
-/*   Updated: 2021/05/20 22:35:04 by gantonio         ###   ########.fr       */
+/*   Created: 2021/05/20 22:27:48 by gantonio          #+#    #+#             */
+/*   Updated: 2021/05/20 22:27:49 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*ptr_str;
-	size_t	i;
-
-	i = 0;
-	ptr_str = (char *)malloc(sizeof(*s) * (len + 1));
-	if (!ptr_str)
-		return (NULL);
-	while (i < len && start < ft_strlen(s))
-		ptr_str[i++] = s[start++];
-	ptr_str[i] = '\0';
-	return (ptr_str);
-}
