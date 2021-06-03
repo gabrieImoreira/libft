296be9d8e3224255d21f7528776ccdbf7d0fbcbd
gams <gabriel.antonioms@gmail.com>
2021-06-03 16:34:01 -0300

    fix lstaddback was not protected

--- sources/ft_lstadd_back.c
+++ sources/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 15:48:23 by gantonio          #+#    #+#             */
-/*   Updated: 2021/06/03 16:29:17 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/06/03 16:33:32 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
 
+	if (!lst|| !new)
+		return ;
+	if (!*lst)
+		*lst = new;
 	tmp = ft_lstlast(*lst);
 	if (tmp)
 		tmp->next = new;
