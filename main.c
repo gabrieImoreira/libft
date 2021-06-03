2d82172dbf04424866903fa520c167511dd55362
gams <gabriel.antonioms@gmail.com>
2021-06-03 15:37:05 -0300

    add lstlast

--- main.c
+++ main.c
@@ -535,9 +535,41 @@ int main () {
 	elem = ft_lstnew((void *)str_a);
 	printf("\n%s\n", elem->content);
    printf("\n************************************\n");
-   // =========================function===================
-   // =========================function===================
-   // =========================function===================
+ // =========================function===================
+ printf("\nft_lstlast:\n");
+	t_list	*elem1;
+	t_list	*elem2;
+	t_list	*elem3;
+	t_list	*elem4;
+	t_list	*elem5;
+	t_list	*elem6;
+	t_list	*ret;
+
+	if(!(elem1 = malloc(sizeof(t_list))))
+		return (0);
+	if(!(elem2 = malloc(sizeof(t_list))))
+		return (0);
+	if(!(elem3 = malloc(sizeof(t_list))))
+		return (0);
+	if(!(elem4 = malloc(sizeof(t_list))))
+		return (0);
+	if(!(elem5 = malloc(sizeof(t_list))))
+		return (0);
+	if(!(elem6 = malloc(sizeof(t_list))))
+		return (0);
+
+	elem1->next = elem2;
+	elem2->next = elem3;
+	elem3->next = elem4;
+	elem4->next = elem5;
+	elem5->next = elem6;
+	elem6->next = NULL;
+
+	elem6->content = (void *)str_a;
+	ret = ft_lstlast(elem1);
+	printf("\n%s\n", (char *)ret->content);
+ // =========================function===================
+ // =========================function===================
    // =========================function===================
 
 }
--- sources/ft_lstlast.c
+++ sources/ft_lstlast.c
@@ -6,8 +6,17 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 15:28:45 by gantonio          #+#    #+#             */
-/*   Updated: 2021/06/03 15:28:51 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/06/03 15:33:30 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_list *ft_lstlast(t_list *lst);
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
